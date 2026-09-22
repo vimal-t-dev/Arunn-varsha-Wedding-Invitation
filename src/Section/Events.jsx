fc6000321b23b2203211a4b22fa043ce842415bb
vimal-t-dev <vimalthirugnanam@gmail.com>
2026-09-22 13:32:45 +0530

    event height changed

--- src/Section/Events.jsx
+++ src/Section/Events.jsx
@@ -7,7 +7,7 @@ import marriageImage from "../assets/Marraige image.jpg";
 function Events() {
   return (
     <section
-      className="relative min-h-screen overflow-hidden bg-cover bg-center px-4 py-20"
+      className="relative max-h-screen overflow-hidden bg-cover bg-center px-4 py-20"
       style={{
         backgroundImage: `url(${eventsBg})`,
       }}
