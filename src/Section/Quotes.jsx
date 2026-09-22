2aafb6c6325a10caf400c0b8846d00278a29aeec
vimal-t-dev <vimalthirugnanam@gmail.com>
2026-09-22 13:09:29 +0530

    fit secton is changed

--- src/Section/Quotes.jsx
+++ src/Section/Quotes.jsx
@@ -94,7 +94,7 @@ function Quotes() {
             transition={{ duration: 0.8, delay: 1.2 }}
             className="mt-1 font-serif text-xl italic text-amber-500"
           >
-            Arunn <span className="mx-1 text-yellow-900">♥</span> Varsha
+            Arunn <span className="mx-1 text-black">♥</span> Varsha
           </motion.p>
         </motion.div>
       </div>
